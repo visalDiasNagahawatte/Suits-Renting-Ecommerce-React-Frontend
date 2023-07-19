c5725c2dabb9f44578f4159f4bd475f6e5e463e1
Visal nagahawatte <visalgayantha76@gmail.com>
2023-07-19 09:34:49 +0530

    new commit

--- react-app/src/pages/ContactPage.jsx
+++ react-app/src/pages/ContactPage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { MDBInput, MDBCheckbox, MDBBtn } from "mdb-react-ui-kit";
 
-function ContactPage() {
-  return <div>ContactPage</div>;
+export default function Contact() {
+  return (
+    <form>
+      <MDBInput id="form4Example1" wrapperClass="mb-4" label="Name" />
+      <MDBInput
+        type="email"
+        id="form4Example2"
+        wrapperClass="mb-4"
+        label="Email address"
+      />
+      <MDBInput
+        wrapperClass="mb-4"
+        textarea
+        id="form4Example3"
+        rows={4}
+        label="Message"
+      />
+
+      <MDBCheckbox
+        wrapperClass="d-flex justify-content-center mb-4"
+        id="form4Example4"
+        label="Send me a copy of this message"
+        defaultChecked
+      />
+
+      <MDBBtn type="submit" className="mb-4" block>
+        Sign in
+      </MDBBtn>
+    </form>
+  );
 }
-
-export default ContactPage;
